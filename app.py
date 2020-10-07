8ddad17dbf8829b47778be6c1cb2944ad9c2c4e6
Nimish Shah <nimishshah2000@gmail.com>
2020-10-07 22:36:51 +0530

    major revamp

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-#importing the necessary libraries
-from flask import Flask,render_template,url_for,request,jsonify
+# importing the necessary libraries
+from flask import Flask, render_template, url_for, request, jsonify
 from flask_restful import reqparse, abort, Api, Resource
-import urllib.request, json
+import urllib.request
+import json
 import os
 import pickle
-import string, os
+import string
+import os
 import warnings
 import numpy as np
 import pandas as pd
@@ -14,34 +16,45 @@ from sklearn import preprocessing
 data = pd.read_csv('crop_data.csv')
 
 
-def generate_crop(state, cost_hectare, cost_quintal,model):
+def generate_crop(state, cost_hectare, cost_quintal, model):
     le = preprocessing.LabelEncoder()
     state_encoded = le.fit_transform(data.State.tolist())
     encoded = le.transform([state])
-    predicted_crop = model.predict([[encoded, int(cost_hectare), int(cost_quintal)]])[0]
+    predicted_crop = model.predict(
+        [[encoded, int(cost_hectare), int(cost_quintal)]])[0]
     return predicted_crop
 
 
-model = pickle.load(open('software_1.pkl','rb'))
+model = pickle.load(open('software_1.pkl', 'rb'))
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def main():
-    return render_template('main.html')
+    return render_template('index.html')
 
-#For displaying predicted value
-@app.route('/predict',methods=['GET', 'POST'])
+# For displaying predicted value
+
+
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
     warnings.filterwarnings("ignore")
-    warnings.simplefilter(action = 'ignore',category = FutureWarning)
-
-    if request.method == 'POST':
-        message1 = request.form['message1']
-        message2 = request.form['message2']
-        message3 = request.form['message3']
-        my_prediction = generate_crop(state = message1,cost_hectare = message2,cost_quintal = message3,model = model)
-    return render_template('main.html',prediction = my_prediction)
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+    if(request.method == 'GET'):
+        return render_template("predict.html")
+    elif request.method == 'POST':
+        message1 = request.form['state']
+        message2 = request.form['costhectare']
+        message3 = request.form['costquintal']
+        try:
+            my_prediction = generate_crop(
+                state=message1, cost_hectare=message2, cost_quintal=message3, model=model)
+        except Exception as e:
+            print(e)
+            return jsonify(predict="Error")
+        return jsonify(predict=my_prediction)
 
 
 if __name__ == '__main__':
-  app.run(debug = True, use_reloader = False)
+    app.run(debug=True, use_reloader=False)
